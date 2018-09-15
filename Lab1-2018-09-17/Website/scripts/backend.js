a49421ff0810ddd0d04845181d6c762ad7e8ffc4
Poonam <pkkdg@mail.umkc.edu>
2018-09-15 18:19:34 -0500

    Updated home page with revised version

--- Lab1-2018-09-17/Website/scripts/backend.js
+++ Lab1-2018-09-17/Website/scripts/backend.js
@@ -1,8 +1,31 @@
 var myApp = angular.module ("myApp" , []);
-myApp.controller ('loginController', function ($scope) {
+myApp.controller ('websiteController', function ($scope, $http) {
     
     $scope.initialize = function() {
     }
+    
+    $scope.getRecipe = function() {
+        $scope.searchFood( $scope.recipeName );
+    }
+
+    $scope.searchFood = function( text ) {
+        // Build the URL string for the API call to Nutritionix
+        var nURL = "https://api.nutritionix.com/v1_1/"
+            + "search/" + text + "?results=0:1&fields=*"
+            + "&appId=" + NUTRITIONIX_APPID
+            + "&appKey=" + NUTRITIONIX_APPKEY;
+        console.log( nURL );
+
+        // Use AJAX to get the information
+        $http( { method: "GET", url: nURL } ).then( function successCallback( response ) {
+            console.log( response );
+            // Copy the information into scope variables.
+            $scope.calories = response["data"]["hits"][0]["fields"]["nf_calories"];
+            $scope.food = response["data"]["hits"][0]["fields"]["item_name"];
+
+            $( "#food-info" ).fadeIn( "fast" );
+        } );
+    };
 
 } );
 
