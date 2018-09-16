0541553c4f06d5ff47261029c7ed01fbc950649d
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2018-09-15 21:48:50 -0500

    Activity suggestion API

--- Lab1-2018-09-17/Website/scripts/angular.js
+++ Lab1-2018-09-17/Website/scripts/angular.js
@@ -2,6 +2,9 @@ var myApp = angular.module ("myApp" , []);
 myApp.controller ('websiteController', function ($scope, $http) {
     
     $scope.initialize = function() {
+        console.log( "Initialize angular" );
+        $scope.getActivity();
+        window.setInterval( $scope.getActivity, 10000 );
     }
     
     $scope.getRecipe = function() {
@@ -27,43 +30,20 @@ myApp.controller ('websiteController', function ($scope, $http) {
         } );
     };
 
-} );
+    
+    $scope.getActivity = function() {
+        console.log( "Get activity" );
+        // https://www.boredapi.com/api/activity/
+        var url = "https://www.boredapi.com/api/activity";
+        console.log( url );
+        
+        // Use AJAX to get the information
+        $http( { method: "GET", url: url } ).then( function successCallback( response ) {
+            console.log( response );
 
-
-$( document ).ready( function() {
-
-    // Not logged in
-    /*
-    if ( hello.getAuthResponse('google') == null && !path.includes( "index.html" ) )
-    {
-        window.location.replace( "index.html" );
-    }
-    * */
-            
-    hello.init({
-        google: "545640970176-mqql7jmfsridgm319n3fatpfct7ieijs.apps.googleusercontent.com"
-    });
-
-    hello.on('auth.login', function (auth) {
-        hello(auth.network).api('/me').then(function (resp) {
-            console.log( "Logged in" );
-            var path = window.location.pathname;
-            var host = window.location.hostname;
-            console.log( path, host );
-
-            if ( path.includes( "index.html" ) || path == "/CS5551/" )
-            {
-                window.location.replace( "home.html" );
-            }
-            else
-            {
-                $( "#login-info" ).html( resp.name + "<img src='" + resp.thumbnail + "'>" );
-            }
-        });
-    });
-
-    hello.on('auth.logout', function () {
-        window.location.replace( "index.html" );
-    });
+            $( "#activity-suggestion" ).html( response["data"]["activity"] );
+            $( "#activity-suggestion" ).fadeIn( "fast" );
+        } );
+    };
 
 } );
